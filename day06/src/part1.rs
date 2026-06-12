8e0473b0a0e3555e0517dfee67bc64a760df5d5b
TimVanMourik <timvanmourik@gmail.com>
2026-06-12 20:34:08 +0000

    day 6

--- day06/src/part1.rs
+++ day06/src/part1.rs
@@ -1,14 +1,42 @@
 use crate::{Computation, Operator};
 
-pub fn part1(items: &[Computation]) -> i64 {
-    items
-        .iter()
-        .map(|comp| {
-            let result: i64 = match comp.operator {
-                Operator::Add => comp.values.iter().sum(),
-                Operator::Multiply => comp.values.iter().product(),
-            };
-            result
-        })
-        .sum()
+pub fn parse(input: &str) -> Vec<Computation> {
+    let mut computation = Vec::new();
+    let lines = input
+        .lines()
+        .filter(|line| !line.trim().is_empty())
+        .map(|line| line.split_whitespace().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    // Check that all lines are of equal length
+    let line_length = lines.first().map_or(0, |line| line.len());
+    if !lines.iter().all(|line| line.len() == line_length) {
+        panic!("All lines must have the same number of elements");
+    }
+    for i in 0..line_length {
+        let mut values = Vec::new();
+        for j in 0..lines.len() - 1 {
+            let value = lines[j][i].parse::<i64>().unwrap_or_else(|_| {
+                panic!("Failed to parse '{}' as i64 in line {}", lines[j][i], j + 1)
+            });
+            values.push(value);
+        }
+        let operator_str = lines.last().unwrap()[i];
+        let operator = match operator_str {
+            "+" => Operator::Add,
+            "*" => Operator::Multiply,
+            _ => panic!(
+                "Invalid operator '{}' in line {}",
+                operator_str,
+                lines.len()
+            ),
+        };
+        computation.push(Computation { values, operator });
+    }
+
+    computation
+}
+
+pub fn part1(input: &String) -> i64 {
+    parse(&input).iter().map(|comp| comp.compute()).sum()
 }
--- day06/src/part2.rs
+++ day06/src/part2.rs
@@ -1,5 +1,88 @@
-use crate::Computation;
+use crate::{Computation, Operator};
 
-pub fn part2(_items: &[Computation]) -> i64 {
-    0
+struct Operation {
+    operator: Operator,
+    index: usize,
+}
+
+fn char_to_operation(char: char, index: usize) -> Option<Operation> {
+    match char {
+        '+' => Some(Operation {
+            operator: Operator::Add,
+            index,
+        }),
+        '*' => Some(Operation {
+            operator: Operator::Multiply,
+            index,
+        }),
+        ' ' => None, // Skip empty operator
+        _ => panic!("Invalid operator '{}'", char,),
+    }
+}
+
+fn right_pad(line: &str, longest: usize) -> String {
+    let mut padded = line.to_string();
+    while padded.len() < longest {
+        padded.push(' ');
+    }
+    padded
+}
+
+pub fn parse(input: &str) -> Vec<Computation> {
+    let mut computation = Vec::new();
+    let lines = input
+        .lines()
+        .filter(|line| !line.trim().is_empty())
+        .collect::<Vec<_>>();
+
+    let line_length = lines.iter().map(|l| l.len()).max().unwrap_or(0);
+    // Pad shorter lines with spaces
+    let lines = lines
+        .into_iter()
+        .map(|line| right_pad(line, line_length))
+        .collect::<Vec<_>>();
+
+    // Find all operatotrs on last line with their indices
+    let operations = lines
+        .last()
+        .unwrap()
+        .chars()
+        .enumerate()
+        .filter_map(|(index, char)| char_to_operation(char, index))
+        .collect::<Vec<_>>();
+
+    // loop through enumartion of operations
+    for operation in operations {
+        let mut values = Vec::new();
+        let mut index = 0;
+        loop {
+            let mut value = String::new();
+            for line in &lines[..lines.len() - 1] {
+                value.push_str(
+                    &line
+                        .chars()
+                        .nth(operation.index + index)
+                        .unwrap_or_else(|| ' ')
+                        .to_string(),
+                );
+            }
+            if value.trim().is_empty() {
+                break;
+            } else {
+                values.push(value.trim().parse::<i64>().unwrap_or_else(|_| {
+                    panic!("Failed to parse '{}' as i64 in line {}", value, index + 1)
+                }));
+            }
+            index += 1;
+        }
+        computation.push(Computation {
+            values,
+            operator: operation.operator,
+        });
+    }
+    computation
+}
+
+pub fn part2(input: &String) -> i64 {
+    parse(&input).iter().map(|comp| comp.compute()).sum()
 }
