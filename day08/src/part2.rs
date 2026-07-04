88f327a9b7104847d9ccd5ee03037faf6b51f5dd
TimVanMourik <timvanmourik@gmail.com>
2026-07-04 12:43:33 +0000

    day 8 part 2

--- day08/src/part2.rs
+++ day08/src/part2.rs
@@ -9,7 +9,7 @@ impl Playground {
         let mut main_circuit = Circuit::new();
         let mut it = self.distance_matrix().shortest_distances();
         let mut last_connected = (0, 0);
-        while main_circuit.len() < self.distance_matrix().distances.len() - 1 {
+        while main_circuit.len() < self.distance_matrix().distances.len() {
             let Some((_d, i, j)) = it.next() else {
                 return last_connected;
             };
@@ -23,8 +23,11 @@ impl Playground {
                 main_circuit.insert(i);
                 main_circuit.insert(j);
             } else {
+                if main_circuit.contains(&i) && main_circuit.contains(&j) {
+                    continue;
+                }
                 if main_circuit.contains(&i) || main_circuit.contains(&j) {
-                    let circuit = connections.find_circuit(i, &mut main_circuit);
+                    let circuit = connections.find_circuit(i, &mut Circuit::new());
                     main_circuit.extend(circuit);
                     last_connected = (i, j);
                 }
@@ -38,15 +41,8 @@ pub fn part2(items: &[JunctionBox]) -> usize {
     let playground = Playground::new(items);
     let last_connected = playground.wire_all();
 
-    println!(
-        "Last connected junction boxes: {:?}, {:?}",
-        last_connected.0, last_connected.1
-    );
-
-    // print their x coordinates
     let x1 = items[last_connected.0].location.0;
     let x2 = items[last_connected.1].location.0;
-    println!("Their x coordinates: {:?}, {:?}", x1, x2);
 
     x1 * x2
 }
